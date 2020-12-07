39e3b14b6810084fd9ed421f49417d35ca627297
M JAYANTH VARMA <jayanthvarma134@gmail.com>
2020-12-07 23:55:37 +0530

    frame flip correction, contours

--- ball tracking/ball_tracking.py
+++ ball tracking/ball_tracking.py
@@ -14,7 +14,7 @@ ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size(tr
 args = vars(ap.parse_args())
 
 
-greenLover = (29, 86, 6)
+greenLower = (29, 86, 6)
 greenUpper = (64, 255, 255)
 pts = deque(maxlen = args["buffer"])
 
@@ -35,26 +35,30 @@ while True:
         break
 
     frame = imutils.resize(frame, width=600)
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    flipped_frame = cv2.flip(frame, 1)
+    blurred = cv2.GaussianBlur(flipped_frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-    mask = cv2.inRange(hsv, greenLover, greenUpper)
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
+    # print(cnts)
     center = None
 
     if len(cnts)>0:
         c = max(cnts, key=cv2.contourArea)
+        # print("this is the max:" ,c)
         ((x,y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
         center = (int(M["m10"]/M["m00"]), int(M["m01"]/M["m00"]))
 
         if radius > 10:
-            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            cv2.drawContours(flipped_frame, cnts, -1, (0,255,0), 3)
+            cv2.circle(flipped_frame, (int(x), int(y)), int(radius), (255, 255, 255), 2)
+            cv2.circle(flipped_frame, center, 5, (0, 0, 255), -1)
 
     pts.appendleft(center)
 
@@ -63,9 +67,9 @@ while True:
             continue
         
         thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i-1], pts[i], (0, 0, 255), thickness)
+        cv2.line(flipped_frame, pts[i-1], pts[i], (0, 0, 255), thickness)
 
-    cv2.imshow("Frame", frame)
+    cv2.imshow("Frame", flipped_frame)
     key = cv2.waitKey(1) & 0xFF
 
     if key == ord("q"):
